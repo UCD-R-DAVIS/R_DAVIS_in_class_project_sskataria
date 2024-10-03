46f3bf054ed94e6228a74681b65bffbeb6eddf86
sskataria <sskataria@ucdavis.edu>
2024-10-04 02:30:55 +0530

    wee 2 work in class

---
+++ Week 2 Intro 2 R.R
@@ -0,0 +1,22 @@
+4+8
+4+8
+4+883
+
+4+(9*3)  
+2/100000000
+mynumber <-6
+mynumber ==5
+
+mynumber
+mynumber !=5
+
+mynumber >4
+object name conventio
+errors and warnings
+
+elephant1kg <-3492
+  elephant2lb <-7757
+
+elephant1_lb <- elephant1_kg * 2.2
+elephant1_lb <- elephant1_kg*2.2
+elephant1_kg <-3492
