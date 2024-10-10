a295466c13caa044230c67b9858da4e8bac20682
sskataria <sskataria@ucdavis.edu>
2024-10-10 06:56:40 +0530

    week 2 in class

--- Week 2 Intro 2 R.R
+++ Week 2 Intro 2 R.R
@@ -5,18 +5,44 @@
 4+(9*3)  
 2/100000000
 mynumber <-6
-mynumber ==5
+getwd()
 
-mynumber
-mynumber !=5
+dir.create("./lectures")
+put # in front it won't run in the script
+weight_g<-c(50,60,65,82)
+weight_one_value<-c(50)
+animals<-c("mouse","rat","dog")
+animals
 
-mynumber >4
-object name conventio
-errors and warnings
+### inspections---
 
-elephant1kg <-3492
-  elephant2lb <-7757
+length(weight_g)
+str(weight_g)
 
-elephant1_lb <- elephant1_kg * 2.2
-elephant1_lb <- elephant1_kg*2.2
-elephant1_kg <-3492
+### change vector ---
+weight_g<-c(weight_g,90)
+
+### challenge---
+num_char<-c(1,2,3,"a") # choses lowest common denominator, avector have to choose the same class of function---
+
+num_logical<-c(1,2,3,TRUE)
+char_logical<-c("a","b","c",TRUE)
+tricky<-c(1,2,3,"4")
+
+
+##subsetting--
+anumals<-c("mouse","rat","dog","cat")
+animals<-c("mouse","rat","dog","cat")
+animals[2]
+
+
+##conditional subsetting--
+weight_g<-c(21,34,39,54,55)
+weight_g>50
+animals[c(2,3,3,2)]
+
+##Symbols
+
+#%in%
+animals%in%c("rat","dog","duck")
+#order matter, diff from teh origina;l script
